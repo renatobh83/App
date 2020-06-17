4dcda8518178ea99c159aeff2f39fab276c5bfc4
renato <renatobh83@icloud.com>
2020-06-16 22:08:53 -0300

    sequelize

---
+++ backend/src/app/models/users.js
@@ -0,0 +1,15 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define(
+    "Users",
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {}
+  );
+  Users.associate = function (models) {
+    // associations can be defined here
+  };
+  return Users;
+};
